2f5cead61e69baa7cc2c16c003efe4b2fabeaa21
Vadim Kantorov <vadimkantorov@gmail.com>
2020-03-12 16:36:23 +0000

    ...

--- README.md
+++ README.md
@@ -3,22 +3,26 @@
 ffmpeg -f lavfi -i "sine=frequency=1000:duration=5" -c:a pcm_s16le -ar 8000 test.wav
 
 # convert audio to raw format
-ffmpeg -i test.wav -f s16le -acodec pcm_s16le out0.raw
+ffmpeg -i test.wav -f s16le -acodec pcm_s16le golden.raw
+
+# play a raw file
+ffplay -f s16le -ac 1 -ar 8000 golden.raw
 
 # compile executable for testing
 gcc -o decode_audio decode_audio.c -lavformat -lavcodec -lswresample -lavutil
 
 # convert audio to raw format and compare to etalon
-./decode_audio test.wav out1.raw
-diff out0.raw out1.raw
-
-# play a raw file
-ffplay -f s16le -ac 1 -ar 8000 out1.raw
+./decode_audio test.wav bin.raw
+diff golden.raw bin.raw
 
 # compile a shared library for interfacing with NumPy
 gcc -o decode_audio.so -shared -fPIC decode_audio.c -lavformat -lavcodec -lswresample -lavutil
 
-# convert audio to raw format and compare to etalon
-python3 decode_audio.py test.wav out2.raw
-diff out0.raw outw.raw
+# convert audio to raw format (NumPy) and compare to etalon
+python3 decode_audio.py test.wav numpy.raw
+diff golden.raw numpy.raw
+
+# convert audio to raw format (PyTorch) and compare to etalon
+python3 decode_audio.py test.wav torch.raw
+diff golden.raw torch.raw
 ```
--- decode_audio.py
+++ decode_audio.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import ctypes
-import numpy as np
 
 class DecodeAudio(ctypes.Structure):
 	_fields_ = [
@@ -15,22 +14,40 @@ class DecodeAudio(ctypes.Structure):
 
 	@property
 	def dtype(self):
-		return 'int16' if self.fmt == b's16le' else 'uint8'
+		return 'int16' if b's16' in self.fmt else 'float32' if 'f32' in self.fmt else 'uint8'
 
 	@property
 	def nbytes(self):
 		return self.num_channels * self.num_samples * self.itemsize
 
-	def __init__(self, lib):
-		self.lib = lib
+	@property
+	def byte_order(self):
+		return 'little' if b'le' in self.fmt else 'big' if b'be' in self.fmt else 'native'
+
+	def __init__(self, lib_path = os.path.abspath('decode_audio.so')):
+		self.lib = ctypes.CDLL(lib_path)
 		self.lib.decode_audio.restype = DecodeAudio
 
 	def __call__(self, input_path):
 		return self.lib.decode_audio(input_path.encode())
 
-decode_audio = DecodeAudio(ctypes.CDLL(os.path.abspath('decode_audio.so')))
-audio = decode_audio(sys.argv[1])
-print('ffplay', '-f', audio.fmt, '-ac', audio.num_channels, '-ar', audio.sample_rate, '-i', sys.argv[1], '# num samples:', audio.num_samples)
+if __name__ == '__main__':
+	decode_audio = DecodeAudio()
 
-arr = np.ctypeslib.as_array(audio.data, shape = (audio.nbytes, )).view(audio.dtype).reshape(audio.num_samples, audio.num_channels)
-arr.tofile(sys.argv[2])
+	audio = decode_audio(sys.argv[1])
+	print('ffplay', '-f', audio.fmt, '-ac', audio.num_channels, '-ar', audio.sample_rate, '-i', sys.argv[1], '# num samples:', audio.num_samples)
+
+	import numpy
+	
+	if 'numpy' in sys.argv[2]:
+		array = numpy.ctypeslib.as_array(audio.data, shape = (audio.nbytes, )).view(audio.dtype).reshape(audio.num_samples, audio.num_channels)
+
+	elif 'torch' in sys.argv[2]:
+		import torch
+		array_ctypes = ctypes.cast(audio.data, ctypes.POINTER(ctypes.c_ubyte * audio.nbytes)).contents
+		dtype2storage = dict(uint8 = torch.CharStorage, int16 = torch.ShortStorage, float32 = torch.FloatStorage)
+		dtype2tensor = dict(uint8 = torch.CharTensor, int16 = torch.ShortTensor, float32 = torch.FloatTensor)
+		array = dtype2tensor[audio.dtype](dtype2storage[audio.dtype].from_buffer(array_ctypes, byte_order = audio.byte_order)).reshape(audio.num_samples, audio.num_channels)
+
+	print(array.dtype, array.shape)
+	numpy.asarray(array).tofile(sys.argv[2])
